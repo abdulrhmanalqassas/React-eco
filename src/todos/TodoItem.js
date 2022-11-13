1b4a8a5eb3bbd624cd9e7b9ef307284d7021acac
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-11-13 21:52:34 +0200

    add COMPLETE_TODO to redux store

--- src/todos/TodoItem.js
+++ src/todos/TodoItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./TodoItem.css"
 
-export default function TodoItem({ todo,onRemove }) {
+export default function TodoItem({ todo,onRemove,onCompleted }) {
   console.log("TodoItem",todo)
   return (
     <div className="todo-item-container">
       <h1>{todo.text}</h1>
       <div className="button-container">
-        <button className="completed-button">Marked as completed </button>
+        {!todo.isCompleted && <button 
+        onClick={()=>onCompleted(todo.text)}
+        className="completed-button">Marked as completed </button>}
         <button
         onClick={()=>onRemove(todo.text)}
         className="remove-button"> Remove </button>
--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -2,17 +2,17 @@ import React from "react";
 import NewTodoForm from "./NewTodoForm";
 import TodoItem from "./TodoItem";
 import { connect } from "react-redux";
-import { removeTodo } from "./actions";
+import { completeTodo, removeTodo } from "./actions";
 import "./TodoList.css";
 
-function TodoList({ todos, onRemove }) {
+function TodoList({ todos, onRemove, onCompleted }) {
   console.log("TodoList>>>", todos);
   return (
     <>
       <NewTodoForm />
       <section className="list-wrapper">
         {todos.map((todo) => (
-          <TodoItem todo={todo} onRemove={onRemove} />
+          <TodoItem todo={todo} onCompleted={onCompleted} onRemove={onRemove} />
         ))}
       </section>
     </>
@@ -22,5 +22,6 @@ function TodoList({ todos, onRemove }) {
 const mapStateToProps = (state) => ({ todos: state.todos });
 const mapDispatchToProps = (dispatch) => ({
   onRemove: (text) => dispatch(removeTodo(text)),
+  onCompleted : (text)=>dispatch(completeTodo(text))
 });
 export default connect(mapStateToProps,mapDispatchToProps)(TodoList);
